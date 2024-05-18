0352581341f95f53238c0966e3032836c5ab070d
F0fo <omar.fouad2004@gmail.com>
2024-05-18 18:45:45 +0200

    fixed performer list, spaces between words and remove correct non needed elements

--- Crawler/SummerFestival/SummerFestival/spiders/crawling_spider.py
+++ Crawler/SummerFestival/SummerFestival/spiders/crawling_spider.py
@@ -17,11 +17,13 @@ class CrawlingSpider(CrawlSpider):
             'title': response.css('.subtitle ::text').get(),
             'location': re.sub('(\r\n)+|(\xa0)+|(\n)+|(\t)',' ', ''.join(response.css('.medium-9 p::text ').getall()[-3:])), #Must update to give a location to conerts in the streets
             'date_time': re.sub('(\n)+|(\t)+','',''.join(response.css('.subtitle ::text').getall()[3].split('|')[:2])),
-            'artist': list(filter(lambda isempty: len(isempty) > 0,map(lambda remove: re.sub('(\n)+|(\t)+|(  +)', '', remove), response.css('.performers-list ::text').getall())))[1:],
-            'works': list(filter(lambda isempty: len(isempty) > 0,map(lambda remove: re.sub('(\n)+|(\t)+|(  +) |(\r)+ | (\xa0)+', '', remove), response.css('.cell .medium-9 ::text').getall())))[:-6],
+            'artist': list(filter(lambda isempty: len(isempty) > 0,map(lambda remove: re.sub('(\n)+|(\t)+|(  +)', '', remove), response.css('.performers-list ::text').getall()))),
+            'works': list(filter(lambda isempty: len(isempty) > 1,map(lambda remove: re.sub('(\n)+|(\t)+|(  +) |(\r)+ | (\xa0)+', '', remove), response.css('.cell .medium-9 ::text').getall())))[:-5]   ,
             'image_link': 'www.lucernefestival.ch' + response.xpath('//picture').get().split('<img src=')[1].lstrip().split(' ')[0][1:-1]
 
 ,
         }
+#
+
 
 
